35b85fb206f4a7f62dd8b7bcc5408f22ca0891ec
Ecce_Li <Ecce_Li@asus.com>
2017-08-22 20:45:00 +0800

    Implement c_gpio.* by wiringTB.*
    Change-Id: I96af302ff8d4a85283c0c5d499b89abcbd23bf5b

--- source/c_gpio.c
+++ source/c_gpio.c
@@ -31,18 +31,6 @@ SOFTWARE.
 #include "common.h"
 #include "wiringTB.h"
 
-static int  mem_fd0;
-static void* gpio_map0[GPIO_BANK];
-static volatile unsigned* gpio0[GPIO_BANK];
-
-static int  mem_fd4;
-static void *grf_map;
-static volatile unsigned *grf;
-
-static int  mem_fdpmu;
-static void *pmu_map;
-static volatile unsigned *pmu;
-
 void short_wait(void)
 {
     int i;
@@ -55,72 +43,7 @@ void short_wait(void)
 
 int setup(void)
 {
-	int i;
-	for(i=0;i<GPIO_BANK;i++)
-	{
-		if ((mem_fd0 = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) 
-		{
-		printf("can't open /dev/mem \n");
-		return SETUP_DEVMEM_FAIL;
-		}
-
-		// mmap GPIO 
-		gpio_map0[i] = mmap(
-		NULL,             // Any adddress in our space will do 
-		PAGE_SIZE,       // Map length 
-		PROT_READ|PROT_WRITE, // Enable reading & writting to mapped memory 
-		MAP_SHARED,       // Shared with other processes 
-		mem_fd0,           // File to map 
-		RK3128_GPIO(i)         //Offset to GPIO peripheral 
-		);
-
-		if ((uint32_t)gpio_map0[i] < 0)
-		return SETUP_MMAP_FAIL;
-		close(mem_fd0); // No need to keep mem_fd open after mmap 
-		gpio0[i] = (volatile unsigned *)gpio_map0[i];
-	}
-
-
-	if((mem_fd4 = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) 
-	{
-		printf("can't open /dev/mem \n");
-		return SETUP_DEVMEM_FAIL;
-	}
-
-	// mmap GPIO 
-	grf_map = mmap(
-		NULL,             // Any adddress in our space will do 
-		PAGE_SIZE,       // Map length 
-		PROT_READ|PROT_WRITE, // Enable reading & writting to mapped memory 
-		MAP_SHARED,       // Shared with other processes 
-		mem_fd4,           // File to map 
-		RK3128_GRF_PHYS         //Offset to GPIO peripheral 
-		);
-	
-	if ((uint32_t)grf_map < 0)
-		return SETUP_MMAP_FAIL;
-	close(mem_fd4); // No need to keep mem_fd open after mmap
-	grf = (volatile unsigned *)grf_map;
-
-	if((mem_fdpmu = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
-		printf("fdpmu can't open /dev/mem \n");
-		return SETUP_DEVMEM_FAIL;
-    	}
-	pmu_map = mmap(
-                NULL,             // Any adddress in our space will do 
-                PAGE_SIZE,       // Map length 
-                PROT_READ|PROT_WRITE, // Enable reading & writting to mapped memory 
-                MAP_SHARED,       // Shared with other processes 
-                mem_fdpmu,           // File to map 
-                RK3288_PMU         //Offset to GPIO peripheral 
-                );
-
-	if ((uint32_t)pmu_map < 0)
-        	return SETUP_MMAP_FAIL;
-    	close(mem_fdpmu); // No need to keep mem_fdpmu open after mmap
-    	pmu = (volatile unsigned *)pmu_map;
-	tinker_board_setup(0);
-	return SETUP_OK;
+	return tinker_board_setup(0);
 }
 
 void set_pullupdn(int gpio, int pud)
--- source/c_gpio.h
+++ source/c_gpio.h
@@ -36,131 +36,23 @@ void cleanup(void);
 #define SETUP_DEVMEM_FAIL 1
 #define SETUP_MALLOC_FAIL 2
 #define SETUP_MMAP_FAIL   3
+// Pin modes
 
-#define INPUT  0 // is really 0 for control register!
-#define OUTPUT 1 // is really 1 for control register!
-#define ALT0   4
+#define	INPUT			 0
+#define	OUTPUT			 1
+#define	PWM_OUTPUT		 2
+#define	GPIO_CLOCK		 3
+#define	SOFT_PWM_OUTPUT		 4
+#define	SOFT_TONE_OUTPUT	 5
+#define	PWM_TONE_OUTPUT		 6
 
-#define HIGH 1
-#define LOW  0
+#define	LOW			 0
+#define	HIGH			 1
 
-#define PUD_OFF  0
-#define PUD_DOWN 1
-#define PUD_UP   2
+// Pull up/down/none
+
+#define	PUD_OFF			 0
+#define	PUD_DOWN		 1
+#define	PUD_UP			 2
+
 #define RK3288
-#ifdef RK3288
-#define RK3288_GPIO0 		0xff750000
-#define RK3288_GPIO1 		0xff780000
-#define RK3288_GPIO2 		0xff790000
-#define RK3288_GPIO3 		0xff7A0000
-#define RK3288_GPIO4 		0xff7B0000
-#define RK3288_GPIO5 		0xff7C0000
-#define RK3288_GPIO6 		0xff7D0000
-#define RK3288_GPIO7 		0xff7E0000
-#define RK3288_GPIO8 		0xff7f0000
-
-#define GPIO_LENGTH 		0x00010000
-#define GPIO_CHANNEL 		0x00020000
-#define GPIO_BASE			0xff750000
-		
-#define GPIO_BANK			9
-
-#define GRF_BASE			0XFF770000
-#define RK3128_GRF_PHYS 	GRF_BASE //this is in use
-#define RK3288_PMU		0xff730000
-
-#define PMU_GPIO0C_IOMUX	0x008C
-#define PMU_GPIO0C_P		0x006C
-
-#define GRF_GPIO5B_IOMUX	0x0050
-#define GRF_GPIO5C_IOMUX	0x0054
-#define GRF_GPIO6A_IOMUX	0x005C
-#define GRF_GPIO7A_IOMUX	0x006c
-#define GRF_GPIO7B_IOMUX	0x0070
-#define GRF_GPIO7CL_IOMUX	0x0074
-#define GRF_GPIO7CH_IOMUX	0x0078
-#define GRF_GPIO8A_IOMUX	0x0080
-#define GRF_GPIO8B_IOMUX	0x0084
-
-#define GRF_GPIO5B_P	0x0184
-#define GRF_GPIO5C_P	0x0188
-#define GRF_GPIO6A_P	0x0190
-#define GRF_GPIO6B_P	0x0194
-#define GRF_GPIO6C_P	0x0198
-#define GRF_GPIO7A_P	0x01A0
-#define GRF_GPIO7B_P	0x01A4
-#define GRF_GPIO7C_P	0x01A8
-#define GRF_GPIO8A_P	0x01B0
-#define GRF_GPIO8B_P	0x01b4
-
-
-
-
-
-
-#else
-#define GPIO_LENGTH			0x00004000
-#define GPIO_CHANNEL 		0
-#define GPIO_BASE			0x2007c000
-#define GPIO_BANK			4
-#define GRF_BASE			0x20000000
-#define RK3128_GRF_PHYS 	(GRF_BASE+0x8000) //this is in use
-#define GRF_GPIO0A_IOMUX 	(0x00a8)	//this is in use 
-#endif
-
-
-
-
-
-
-
-
-//#define RK3128_GPIO0		0x2007c000
-//#define RK3128_GPIO1		0x20080000
-//#define RK3128_GPIO2		0x20084000
-//#define RK3128_GPIO3		0x20088000
-
-#define RK3128_GPIO(x)		(GPIO_BASE+x*GPIO_LENGTH+(x>0)*GPIO_CHANNEL)
-#define RK3368_GPIO(x)		(0xff780000+x*0x00010000)
-#define RK3368_GRF_PHYS		(0xff770000)
-#define RK3368_GRF_GPIO1A_IOMUX	(0x0000)
-#define RK3368_GRF_GPIO1B_IOMUX	(0x0004)
-#define RK3368_GRF_GPIO1C_IOMUX	(0x0008)
-#define RK3368_GRF_GPIO1D_IOMUX	(0x000c)
-#define RK3368_GRF_GPIO2A_IOMUX	(0x0010)
-#define RK3368_GRF_GPIO2B_IOMUX	(0x0014)
-#define RK3368_GRF_GPIO2C_IOMUX	(0x0018)
-#define RK3368_GRF_GPIO2D_IOMUX	(0x001c)
-#define RK3368_GRF_GPIO3A_IOMUX	(0x0020)
-#define RK3368_GRF_GPIO3B_IOMUX	(0x0024)
-#define RK3368_GRF_GPIO3C_IOMUX	(0x0028)
-#define RK3368_GRF_GPIO3D_IOMUX	(0x002c)
-#define RK3368_GRF_GPIO1A_P	(0x0100)	//this is in use but not imply
-#define RK3368_GRF_GPIO1B_P	(0x0104)
-#define RK3368_GRF_GPIO1C_P	(0x0108)
-#define RK3368_GRF_GPIO1D_P	(0x010c)
-#define RK3368_GRF_GPIO2A_P	(0x0110)
-#define RK3368_GRF_GPIO2B_P	(0x0114)
-#define RK3368_GRF_GPIO2C_P	(0x0118)
-#define RK3368_GRF_GPIO2D_P	(0x011c)
-#define RK3368_GRF_GPIO3A_P	(0x0120)
-#define RK3368_GRF_GPIO3B_P	(0x0124)
-#define RK3368_GRF_GPIO3C_P	(0x0128)
-#define RK3368_GRF_GPIO3D_P	(0x012c)
-#define GPIO_SWPORTA_DR_OFFSET		0x0000	//this is in use write data
-#define	GPIO_SWPORTA_DDR_OFFSET		0x0004  //this is in use direction
-#define GPIO_INTEN_OFFSET		0x0030		
-#define GPIO_INTMASK_OFFSET		0x0034
-#define GPIO_INTTYPE_LEVEL_OFFSET	0x0038
-#define GPIO_INT_POLARITY_OFFSET	0x003c
-#define GPIO_INT_STATUS_OFFSET		0x0040
-#define GPIO_INT_RAWSTATUS_OFFSET	0x0044
-#define GPIO_DEBOUNCE_OFFSET		0x0048	
-#define GPIO_PORTA_EOI_OFFSET		0x004c
-#define GPIO_EXT_PORTA_OFFSET		0x0050  //this is in use read data
-#define GPIO_LS_SYNC_OFFSET		0x0060
-
-
-
-#define PAGE_SIZE  (4*1024)
-#define BLOCK_SIZE (4*1024)
